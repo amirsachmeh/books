2d680f4cc4595ca682c7f9e29d1b48505c92b53f
amirsachmeh <amirhs.dma@gmail.com>
2023-08-15 23:39:04 +0330

    contextAdadStringVa

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import  './index.css';
 import React from 'react';
 import ReactDom from "react-dom/client";
 import App from './App';
-
+import BooksContext from './context/books';
   const el = document.getElementById('root');
   const root = ReactDom.createRoot(el);
 
 
-  root.render(<App/>)
+  root.render(
+  <BooksContext.Provider value={5}>
+  <App  />
+  </BooksContext.Provider>
+  );
