f624936102299826e6203dbf5ea34f63ad3eacf9
amirsachmeh <amirhs.dma@gmail.com>
2023-08-17 00:34:17 +0330

    contextStateTest

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,13 +2,14 @@ import { useContext } from "react";
 import BooksContext from "../context/books";
 import BookShow from "./BookShow"
 function BookList({books,onDelete,onEdit}) {
-    const  value = useContext(BooksContext)
+    const {count,incrementCount} = useContext(BooksContext)
 
     const renderedBooks = books.map((book) => {
         return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book}/>;
     });
     return( <div className="book-list">
-        {value}
+        {count}
+        <button onClick={incrementCount}>Click</button>
         {renderedBooks}
         </div>
         );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import  './index.css';
 import React from 'react';
 import ReactDom from "react-dom/client";
 import App from './App';
-import BooksContext from './context/books';
+import { Provider } from './context/books';
+
   const el = document.getElementById('root');
   const root = ReactDom.createRoot(el);
 
 
   root.render(
-  <BooksContext.Provider value={5}>
-  <App  />
-  </BooksContext.Provider>
+    <Provider>
+      <App/>
+    </Provider>
+
   );
